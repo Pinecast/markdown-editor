5803a3333170f6138990a3a7a25337d084aa74e6
Matt Basta <me@mattbasta.com>
2016-10-16 18:00:43 -0700

    Remove spurious css file

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
-import fromTextEditor from './Editor';
+'use strict';
 
-const elems = document.querySelectorAll('textarea.markdown-editor');
-Array.from(elems).forEach(fromTextEditor);
+var _Editor = require('./Editor');
+
+var _Editor2 = _interopRequireDefault(_Editor);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var elems = document.querySelectorAll('textarea.markdown-editor');
+Array.from(elems).forEach(_Editor2.default);
 //# sourceMappingURL=index.js.map
--- dist/vendor/continuelist.js
+++ dist/vendor/continuelist.js
@@ -1,51 +1,48 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
+"use strict";
+
+var _codemirror = require("codemirror");
+
+var _codemirror2 = _interopRequireDefault(_codemirror);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var listRE = /^(\s*)(>[> ]*|[*+-]\s|(\d+)\.)(\s*)/,
+    emptyListRE = /^(\s*)(>[> ]*|[*+-]|(\d+)\.)(\s*)$/,
+    unorderedListRE = /[*+-]\s/; // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
-(function (mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("codemirror"));else if (typeof define == "function" && define.amd) // AMD
-    define(["codemirror"], mod);else // Plain browser env
-    mod(CodeMirror);
-})(function (CodeMirror) {
-  "use strict";
+_codemirror2.default.commands.newlineAndIndentContinueMarkdownList = function (cm) {
+  if (cm.getOption("disableInput")) return _codemirror2.default.Pass;
+  var ranges = cm.listSelections(),
+      replacements = [];
+  for (var i = 0; i < ranges.length; i++) {
+    var pos = ranges[i].head;
+    var eolState = cm.getStateAfter(pos.line);
+    var inList = eolState.list !== false;
+    var inQuote = eolState.quote !== 0;
 
-  var listRE = /^(\s*)(>[> ]*|[*+-]\s|(\d+)\.)(\s*)/,
-      emptyListRE = /^(\s*)(>[> ]*|[*+-]|(\d+)\.)(\s*)$/,
-      unorderedListRE = /[*+-]\s/;
-
-  CodeMirror.commands.newlineAndIndentContinueMarkdownList = function (cm) {
-    if (cm.getOption("disableInput")) return CodeMirror.Pass;
-    var ranges = cm.listSelections(),
-        replacements = [];
-    for (var i = 0; i < ranges.length; i++) {
-      var pos = ranges[i].head;
-      var eolState = cm.getStateAfter(pos.line);
-      var inList = eolState.list !== false;
-      var inQuote = eolState.quote !== 0;
-
-      var line = cm.getLine(pos.line),
-          match = listRE.exec(line);
-      if (!ranges[i].empty() || !inList && !inQuote || !match) {
-        cm.execCommand("newlineAndIndent");
-        return;
-      }
-      if (emptyListRE.test(line)) {
-        cm.replaceRange("", {
-          line: pos.line, ch: 0
-        }, {
-          line: pos.line, ch: pos.ch + 1
-        });
-        replacements[i] = "\n";
-      } else {
-        var indent = match[1],
-            after = match[4];
-        var bullet = unorderedListRE.test(match[2]) || match[2].indexOf(">") >= 0 ? match[2] : parseInt(match[3], 10) + 1 + ".";
-
-        replacements[i] = "\n" + indent + bullet + after;
-      }
+    var line = cm.getLine(pos.line),
+        match = listRE.exec(line);
+    if (!ranges[i].empty() || !inList && !inQuote || !match) {
+      cm.execCommand("newlineAndIndent");
+      return;
     }
+    if (emptyListRE.test(line)) {
+      cm.replaceRange("", {
+        line: pos.line, ch: 0
+      }, {
+        line: pos.line, ch: pos.ch + 1
+      });
+      replacements[i] = "\n";
+    } else {
+      var indent = match[1],
+          after = match[4];
+      var bullet = unorderedListRE.test(match[2]) || match[2].indexOf(">") >= 0 ? match[2] : parseInt(match[3], 10) + 1 + ".";
 
-    cm.replaceSelections(replacements);
-  };
-});
+      replacements[i] = "\n" + indent + bullet + after;
+    }
+  }
+
+  cm.replaceSelections(replacements);
+};
 //# sourceMappingURL=continuelist.js.map
