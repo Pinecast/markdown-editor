ec75fd0726146799f6d0bbfaf044ed4815bc83ff
Matt Basta <me@mattbasta.com>
2018-01-14 10:09:29 -0800

    Update everything

--- src/Editor.js
+++ src/Editor.js
@@ -9,50 +9,43 @@ import './vendor/continuelist';
 import {Converter} from 'showdown';
 const converter = new Converter();
 
+export default class Editor {
+  constructor(elem) {
+    this.wrapper = document.createElement('div');
+    this.wrapper.className = 'markdown-editor';
 
-class Editor {
-    constructor(elem) {
-        this.wrapper = document.createElement('div');
-        this.wrapper.className = 'markdown-editor';
+    this.previewButton = document.createElement('a');
+    this.previewButton.className = 'markdown-editor-preview-toggle';
+    this.previewButton.innerText = this.previewButton.textContent = 'Toggle Preview';
+    this.previewButton.addEventListener('click', e => {
+      e.preventDefault();
+      e.stopPropagation();
 
-        this.previewButton = document.createElement('a');
-        this.previewButton.className = 'markdown-editor-preview-toggle';
-        this.previewButton.innerText = this.previewButton.textContent = 'Toggle Preview';
-        this.previewButton.addEventListener('click', e => {
-            e.preventDefault();
-            e.stopPropagation();
+      this.wrapper.classList.toggle('previewing');
+    });
+    this.wrapper.appendChild(this.previewButton);
 
-            this.wrapper.classList.toggle('previewing');
-        });
-        this.wrapper.appendChild(this.previewButton);
+    this.preview = document.createElement('div');
+    this.preview.className = 'markdown-editor-preview';
+    this.wrapper.appendChild(this.preview);
 
-        this.preview = document.createElement('div');
-        this.preview.className = 'markdown-editor-preview';
-        this.wrapper.appendChild(this.preview);
+    this.elem = elem;
+    this.elem.parentNode.replaceChild(this.wrapper, this.elem);
+    this.wrapper.appendChild(this.elem);
 
-        this.elem = elem;
-        this.elem.parentNode.replaceChild(this.wrapper, this.elem);
-        this.wrapper.appendChild(this.elem);
+    this.editor = cm.fromTextArea(elem, {
+      mode: 'markdown',
+      lineNumbers: false,
+      matchBrackets: true,
+      lineWrapping: true,
+      theme: 'base16-light',
+      extraKeys: {Enter: 'newlineAndIndentContinueMarkdownList'},
+    });
 
-        this.editor = cm.fromTextArea(elem, {
-            mode: 'markdown',
-            lineNumbers: false,
-            matchBrackets: true,
-            lineWrapping: true,
-            theme: 'base16-light',
-            extraKeys: {Enter: 'newlineAndIndentContinueMarkdownList'},
-        });
+    this.editor.on('change', e => this.update(e.getValue()));
+  }
 
-        this.editor.on('change', e => this.update(e.getValue()));
-        this.update(this.elem.value);
-    }
-
-    update(markdown) {
-        this.preview.innerHTML = converter.makeHtml(markdown);
-    }
+  update(markdown) {
+    this.preview.innerHTML = converter.makeHtml(markdown);
+  }
 }
-
-
-export default function fromTextArea(textarea) {
-    return new Editor(textarea);
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,87 @@
-var path = require('path');
+const path = require('path');
 
-var webpack = require('webpack');
-
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    // devtool: 'source-maps',
-    entry: {
-        app: ['./src/index.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
-        filename: '/markdown-editor.js',
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': '"production"',
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {warnings: false},
-            mangle: {},
-            sourceMap: false,
-        }),
-        new webpack.optimize.DedupePlugin(),
+  // devtool: 'source-maps',
+  entry: {
+    app: ['./src/index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'markdown-editor.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+    new webpack.LoaderOptionsPlugin({minimize: true}),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new MinifyPlugin(
+      {
+        mangle: {
+          blacklist: ['Buffer'],
+        },
+      }
+    ),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
     ],
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader',
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader',
-            },
-        ],
+  },
+  externals: [
+    (context, request, callback) => {
+      switch (request) {
+        // For Node compatibility
+        case 'fs':
+        case 'net':
+          return callback(null, 'null');
+        case 'tty':
+          return callback(null, '{isatty:function() {}}');
+
+        // Removing junk that doesn't need a package.
+        case 'bluebird':
+          // For react-google-charts
+          return callback(null, 'Promise');
+        case 'function-bind':
+          return callback(null, 'Function.prototype.bind');
+        case 'is-array':
+        case 'isarray':
+          return callback(null, 'Array.isArray');
+        case 'is-finite':
+          return callback(
+            null,
+            'function(x) {return typeof x !== "number" && !isNaN(x) && x !== Infinity && x !== -Infinity;}',
+          );
+        case 'is-function':
+          return callback(null, 'function(x) {return typeof x === "function";}');
+        case 'object.assign':
+        case 'object-assign':
+          return callback(null, 'Object.assign');
+        case 'object-keys':
+          return callback(
+            null,
+            '(function() {var x = Object.keys.bind(Object); x.shim = x; return x;}())',
+          );
+
+        default:
+          return callback();
+      }
     },
+  ],
 };
