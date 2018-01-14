c4d6b41cc9ec36478f69d4523eee12cf8d0921aa
Matt Basta <me@mattbasta.com>
2018-01-14 10:13:01 -0800

    Commit uncommitted file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import fromTextEditor from './Editor';
-
+import Editor from './Editor';
 
 const elems = document.querySelectorAll('textarea.markdown-editor');
-Array.from(elems).forEach(fromTextEditor);
+Array.from(elems).forEach(el => {
+  const e = new Editor(el);
+  e.update(el.value);
+});
